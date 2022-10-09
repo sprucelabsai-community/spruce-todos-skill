91df49f22028a56b336b81e045b06c2578b456db
Taylor Romero <me@taylorrome.ro>
2022-10-09 14:16:37 -0600

    patch: first_listener

--- src/__tests__/behavioral/AddTodoListener.test.ts
+++ src/__tests__/behavioral/AddTodoListener.test.ts
@@ -1,19 +1,16 @@
 import { fake } from '@sprucelabs/spruce-test-fixtures'
 import { AbstractSpruceFixtureTest } from '@sprucelabs/spruce-test-fixtures'
-import { test, assert } from '@sprucelabs/test-utils'
+import { test, assert, generateId } from '@sprucelabs/test-utils'
 
 @fake.login()
 export default class AddTodoListenerTest extends AbstractSpruceFixtureTest {
 	@test()
-	protected static async canCreateAddTodoListener() {
-		const addTodoListener = new AddTodoListener()
-		assert.isTruthy(addTodoListener)
-	}
-
-	@test()
-	protected static async yourNextTest() {
-		assert.isTrue(false)
+	protected static async hasAddTodoListener() {
+		await this.bootSkill()
+		await this.fakedClient.emitAndFlattenResponses('todos.add::v2022_10_08', {
+			payload: {
+				todo: generateId(),
+			},
+		})
 	}
 }
-
-class AddTodoListener {}
---
+++ src/listeners/todos/add.v2022_10_08.listener.ts
@@ -0,0 +1,24 @@
+import { SkillEventContract } from '@sprucelabs/mercury-types'
+import {
+	SpruceEvent,
+	SpruceEventResponse,
+} from '@sprucelabs/spruce-event-utils'
+import { generateId } from '@sprucelabs/test-utils'
+import { SpruceSchemas } from '#spruce/schemas/schemas.types'
+
+export default async (
+	event: SpruceEvent<SkillEventContract, EmitPayload>
+): SpruceEventResponse<ResponsePayload> => {
+	return {
+		todo: {
+			id: generateId(),
+			target: {
+				personId: generateId(),
+			},
+			todo: generateId(),
+		},
+	}
+}
+
+type EmitPayload = SpruceSchemas.Todos.v2022_10_08.AddEmitTargetAndPayload
+type ResponsePayload = SpruceSchemas.Todos.v2022_10_08.AddResponsePayload
