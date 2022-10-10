dace9ac0da3f0ff5b51e3e3bfcac929b32d7d014
Ryan Johnson <ryan@sprucelabs.ai>
2022-10-10 09:21:05 -0400

    patch: adding sandbox

--- src/.spruce/events/listeners.ts
+++ src/.spruce/events/listeners.ts
@@ -8,6 +8,12 @@ const listeners: EventFeatureListener[] = [
         callback: require('../../listeners/skill/did-boot.v2022_10_09.listener').default,
     },
     {
+        eventName: 'will-boot',
+        eventNamespace: 'skill',
+        version: 'v2022_10_09',
+        callback: require('../../listeners/skill/will-boot.v2022_10_09.listener').default,
+    },
+    {
         eventName: 'add',
         eventNamespace: 'todos',
         version: 'v2022_10_08',
