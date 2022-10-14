79be4a6d6cda083b9e895435273b77ec76d54481
CircleCI <devops@sprucelabs.ai>
2022-10-14 05:48:25 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/events/listeners.ts
+++ src/.spruce/events/listeners.ts
@@ -1,34 +1,30 @@
 import { EventFeatureListener } from '@sprucelabs/spruce-event-utils'
 
 const listeners: EventFeatureListener[] = [
-	{
-		eventName: 'list',
-		eventNamespace: 'todos',
-		version: 'v2022_10_08',
-		callback: require('../../listeners/todos/list.v2022_10_08.listener')
-			.default,
-	},
-	{
-		eventName: 'did-boot',
-		eventNamespace: 'skill',
-		version: 'v2022_10_09',
-		callback: require('../../listeners/skill/did-boot.v2022_10_09.listener')
-			.default,
-	},
-	{
-		eventName: 'will-boot',
-		eventNamespace: 'skill',
-		version: 'v2022_10_09',
-		callback: require('../../listeners/skill/will-boot.v2022_10_09.listener')
-			.default,
-	},
-	{
-		eventName: 'add',
-		eventNamespace: 'todos',
-		version: 'v2022_10_08',
-		callback: require('../../adding/listeners/todos/add.v2022_10_08.listener')
-			.default,
-	},
+    {
+        eventName: 'did-boot',
+        eventNamespace: 'skill',
+        version: 'v2022_10_09',
+        callback: require('../../listeners/skill/did-boot.v2022_10_09.listener').default,
+    },
+    {
+        eventName: 'will-boot',
+        eventNamespace: 'skill',
+        version: 'v2022_10_09',
+        callback: require('../../listeners/skill/will-boot.v2022_10_09.listener').default,
+    },
+    {
+        eventName: 'list',
+        eventNamespace: 'todos',
+        version: 'v2022_10_08',
+        callback: require('../../listeners/todos/list.v2022_10_08.listener').default,
+    },
+    {
+        eventName: 'add',
+        eventNamespace: 'todos',
+        version: 'v2022_10_08',
+        callback: require('../../adding/listeners/todos/add.v2022_10_08.listener').default,
+    },
 ]
 
 export default listeners
